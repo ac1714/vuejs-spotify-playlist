a124460651667c30f293987d07c74febaa477226
johannesss <johannes.eriksson@hyperisland.se>
2016-01-21 23:40:16 +0100

    mistake in randomizing

--- js/app.js
+++ js/app.js
@@ -91,9 +91,9 @@ module.exports = function() {
 				// triggered when the related artists top tracks have been fetched
 				'topTracksFetched': function(tracks) {
 					var tracks = this.transformTracks(tracks);
-					this.$set('topTracks', this.shuffleArray(tracks));
-					
-					tracks = _.uniqBy(tracks, 'artist_id');
+					tracks = this.shuffleArray(tracks);
+					this.topTracks = _.uniqBy(tracks, 'artist_id');
+
 					this.topTrackIds.uris = this.topTracks.map(function(t) { return t.uri; });
 
 					this.status.loading = false;
